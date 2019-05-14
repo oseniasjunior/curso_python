50bcd1aacec2a839a603ad27790bc226dcbdea10
Osenias Oliveira <oseniasjunior@gmail.com>
2019-05-14 15:33:47 -0400

    Create new models

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 
@@ -19,6 +20,10 @@ class Category(models.Model):
         db_column='cs_active',
         null=False,
         default=True
+    )
+    products = models.ManyToManyField(
+        to='Product',
+        through='ProductCategory'
     )
 
     class Meta:
@@ -43,6 +48,22 @@ class Product(models.Model):
         null=False,
         default=True
     )
+    categories = models.ManyToManyField(
+        to='Category',
+        through='ProductCategory'
+    )
+
+    class Meta:
+        db_table = 'product'
+        managed = True
+
+
+class ProductCategory(models.Model):
+    id = models.AutoField(
+        db_column='id_product_category',
+        null=False,
+        primary_key=True
+    )
     category = models.ForeignKey(
         to='Category',
         on_delete=models.DO_NOTHING,
@@ -50,10 +71,100 @@ class Product(models.Model):
         null=False,
         db_index=False
     )
+    product = models.ForeignKey(
+        to='Product',
+        on_delete=models.DO_NOTHING,
+        db_column='id_product',
+        null=False,
+        db_index=False
+    )
 
     class Meta:
-        db_table = 'product'
+        db_table = 'product_category'
         managed = True
         indexes = [
-            models.Index(fields=['category'])
+            models.Index(fields=['category']),
+            models.Index(fields=['product'])
+        ]
+
+
+class Department(models.Model):
+    id = models.AutoField(
+        db_column='id_department',
+        null=False,
+        primary_key=True
+    )
+    description = models.CharField(
+        db_column='tx_description',
+        null=False,
+        max_length=104,
+        unique=True
+    )
+    active = models.BooleanField(
+        db_column='cs_active',
+        null=False,
+        default=True
+    )
+    created_at = models.DateTimeField(
+        db_column='dt_created_at',
+        null=False,
+        auto_now_add=True
+    )
+    modified_at = models.DateTimeField(
+        db_column='dt_modified_at',
+        null=False,
+        auto_now=True
+    )
+
+    class Meta:
+        db_table = 'department'
+        managed = True
+
+
+class Employee(models.Model):
+    id = models.AutoField(
+        db_column='id_employee',
+        null=False,
+        primary_key=True
+    )
+    name = models.CharField(
+        db_column='tx_name',
+        null=False,
+        max_length=104,
+        unique=True
+    )
+    active = models.BooleanField(
+        db_column='cs_active',
+        null=False,
+        default=True
+    )
+    department = models.ForeignKey(
+        to='Department',
+        on_delete=models.DO_NOTHING,
+        db_column='id_department',
+        null=False,
+        db_index=False,
+        related_name='employees'
+    )
+    created_at = models.DateTimeField(
+        db_column='dt_created_at',
+        null=False,
+        auto_now_add=True
+    )
+    modified_at = models.DateTimeField(
+        db_column='dt_modified_at',
+        null=False,
+        auto_now=True
+    )
+    gender = models.CharField(
+        db_column='cs_gender',
+        null=True,
+        max_length=1
+    )
+
+    class Meta:
+        db_table = 'employee'
+        managed = True
+        indexes = [
+            models.Index(fields=['department'])
         ]
